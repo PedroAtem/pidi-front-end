2cc62f54c12d0642a74b1a8fe673c3682f73f736
pedro <pedro.phdois@gmail.com>
2020-02-04 12:25:22 -0300

    30c4d84

---
+++ precache-manifest.69c28f6befe21a68ad00d492d90a81de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb69c54d63b1d473b8a633b6de16ae7f",
+    "url": "/pidi-front-end/index.html"
+  },
+  {
+    "revision": "c90175ad4186c6c350c0",
+    "url": "/pidi-front-end/static/css/2.da6a58a2.chunk.css"
+  },
+  {
+    "revision": "34d602253415f76164da",
+    "url": "/pidi-front-end/static/css/main.1aea02f6.chunk.css"
+  },
+  {
+    "revision": "c90175ad4186c6c350c0",
+    "url": "/pidi-front-end/static/js/2.e341e8c8.chunk.js"
+  },
+  {
+    "revision": "48b1898183470502aa618c2fb1dd1034",
+    "url": "/pidi-front-end/static/js/2.e341e8c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34d602253415f76164da",
+    "url": "/pidi-front-end/static/js/main.fcc3503b.chunk.js"
+  },
+  {
+    "revision": "b481077efe6eecd25ca6",
+    "url": "/pidi-front-end/static/js/runtime-main.7607974e.js"
+  }
+]);
